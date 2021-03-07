1fce30904a290b1035557e6450b2e95fc7585c74
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2021-03-07 23:56:50 +0100

    Add store class

--- src/store.h
+++ src/store.h
@@ -6,7 +6,21 @@
 
 class Store {
 public:
+    template <class T>
+    void set(const string &key, const T &value) {
+        key_values.insert_or_assign(value);
+    }
+
+    template <class T>
+    T get(const string &name) {
+        auto it = key_values.find(name);
+        if (it == key_values.end()) {
+        }
+        auto &value = it->second;
+        return std::get<T>(value);
+    }
+
 private:
-    using StoreValue = std::variant<i32, f32>;
-    std::map<std::string, StoreValue> keyValues;
+    using Value = std::variant<i32, f32>;
+    std::map<std::string, Value> key_values;
 };
