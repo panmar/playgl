1f5bfd4d68c3ecf5de37f35219a0ab450ca5995e
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2021-05-20 12:02:31 +0200

    Change renderer.h back to classes layout.
    Changing namespace layout to classes layout allows for order
    independent method declaration.
    
    Add render_grid method for RendererGL2

--- src/graphics/renderer.h
+++ src/graphics/renderer.h
@@ -9,82 +9,111 @@
 #include "common.h"
 #include "camera.h"
 
-namespace RendererGL2 {
-
-void set_window_hints() {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-}
-
-void begin_frame(const Store& store) {
-    {
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        auto camera_position =
-            store.get<glm::vec3>(StoreParams::kCameraPosition);
-        auto camera_target = store.get<glm::vec3>(StoreParams::kCameraTarget);
-        auto camera_up = store.get<glm::vec3>(StoreParams::kCameraUp);
-        gluLookAt(camera_position.x, camera_position.y, camera_position.z,
-                  camera_target.x, camera_target.y, camera_target.z,
-                  camera_up.x, camera_up.y, camera_up.z);
-
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(glm::degrees(store.get<f32>(StoreParams::kCameraFov)),
-                       store.get<f32>(StoreParams::kCameraAspectRatio),
-                       store.get<f32>(StoreParams::kCameraNear),
-                       store.get<f32>(StoreParams::kCameraFar));
+class RendererGL2 {
+public:
+    static void set_window_hints() {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     }
 
-    glViewport(0, 0, store.get<u32>(StoreParams::kFrameBufferWidth),
-               store.get<u32>(StoreParams::kFrameBufferHeight));
+    static void begin_frame(const Store& store) {
+        {
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
 
-    glClearColor(0.5f, 0.5f, 0.5f, 1.f);
+            auto camera_position =
+                store.get<glm::vec3>(StoreParams::kCameraPosition);
+            auto camera_target =
+                store.get<glm::vec3>(StoreParams::kCameraTarget);
+            auto camera_up = store.get<glm::vec3>(StoreParams::kCameraUp);
+            gluLookAt(camera_position.x, camera_position.y, camera_position.z,
+                      camera_target.x, camera_target.y, camera_target.z,
+                      camera_up.x, camera_up.y, camera_up.z);
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluPerspective(
+                glm::degrees(store.get<f32>(StoreParams::kCameraFov)),
+                store.get<f32>(StoreParams::kCameraAspectRatio),
+                store.get<f32>(StoreParams::kCameraNear),
+                store.get<f32>(StoreParams::kCameraFar));
+        }
 
-    if (Settings::graphics_multisampling) {
-        glEnable(GL_MULTISAMPLE);
+        glViewport(0, 0, store.get<u32>(StoreParams::kFrameBufferWidth),
+                   store.get<u32>(StoreParams::kFrameBufferHeight));
+
+        glClearColor(0.5f, 0.5f, 0.5f, 1.f);
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+
+        if (Settings::graphics_multisampling) {
+            glEnable(GL_MULTISAMPLE);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
+    static void render_gizmo(const glm::vec3& position) {
+        glBegin(GL_LINES);
 
-void render_gizmo(const glm::vec3& position) {
-    glBegin(GL_LINES);
+        auto edge = 1.f;
 
-    auto edge = 1.f;
+        glLineWidth(8.f);
 
-    glLineWidth(8.f);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(position.x, position.y, position.z);
+        glVertex3f(position.x + edge, position.y, position.z);
 
-    glColor3f(1.f, 0.f, 0.f);
-    glVertex3f(position.x, position.y, position.z);
-    glVertex3f(position.x + edge, position.y, position.z);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(position.x, position.y, position.z);
+        glVertex3f(position.x, position.y + edge, position.z);
 
-    glColor3f(0.f, 1.f, 0.f);
-    glVertex3f(position.x, position.y, position.z);
-    glVertex3f(position.x, position.y + edge, position.z);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(position.x, position.y, position.z);
+        glVertex3f(position.x, position.y, position.z + edge);
 
-    glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(position.x, position.y, position.z);
-    glVertex3f(position.x, position.y, position.z + edge);
+        glLineWidth(1.f);
 
-    glLineWidth(1.f);
+        glEnd();
+    }
 
-    glEnd();
-}
+    static void render_grid(const glm::vec3& position, f32 size,
+                            const glm::vec3& color) {
+        glBegin(GL_LINES);
 
-}  // namespace RendererGL2
+        auto sizeOver2 = size / 2.f;
 
-namespace RendererGL4 {
+        glLineWidth(8.f);
+        glColor3f(color.r, color.g, color.b);
 
-void set_window_hints() {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-}
+        const int count = 10;
+        for (auto i = 0; i <= count; ++i) {
+            glVertex3f(position.x - sizeOver2, position.y,
+                       position.z - sizeOver2 + i * (size / count));
+            glVertex3f(position.x + sizeOver2, position.y,
+                       position.z - sizeOver2 + i * (size / count));
+        }
 
-}  // namespace RendererGL4
+        for (auto i = 0; i <= count; ++i) {
+            glVertex3f(position.x - sizeOver2 + i * (size / count), position.y,
+                       position.z - sizeOver2);
+            glVertex3f(position.x - sizeOver2 + i * (size / count), position.y,
+                       position.z + sizeOver2);
+        }
+
+        glLineWidth(1.f);
+
+        glEnd();
+    }
+};
+
+class RendererGL4 {
+public:
+    static void set_window_hints() {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    }
+};
