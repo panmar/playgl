51f0d7f3fd88f56e9327b5a10c9dc3f6ca1d6e64
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2021-05-25 14:07:05 +0200

    Modify build.bat script
    Change build directory from build/ to bin/
    Add assimp dependency

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-mkdir .\build
-pushd .\build
+mkdir .\bin
+pushd .\bin
 
 cl /MD /std:c++17 ^
 	/EHsc /Zi ^
@@ -9,7 +9,7 @@ cl /MD /std:c++17 ^
 	/DGLFW_EXPOSE_NATIVE_WIN32 ^
 	..\src\main.cc ^
     ..\libs\glad.cc ^
-	kernel32.lib  shell32.lib user32.lib gdi32.lib comdlg32.lib glu32.lib glfw3.lib opengl32.lib ^
+	kernel32.lib  shell32.lib user32.lib gdi32.lib comdlg32.lib glu32.lib glfw3.lib opengl32.lib assimp.lib^
 	/link /LIBPATH:C:\work\projects\playgl\libs
 
 popd
