9afee8e7152a7bce2e15806bed066f3e04ee24d2
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2021-09-21 12:35:48 +0200

    Add blend mode. Change default gpustate values.

--- examples/example.cc
+++ examples/example.cc
@@ -2,11 +2,14 @@
 #include "playgl.h"
 
 void pgl_init(Store& store) {
+    store["screen_color"] = Color(0.5f, 0.5f, 0.5f);
     store["PHONG_COLOR"] = Color(0.7f, 0.4f, 0.3f);
     store["LIGHT_COLOR"] = Color(0.8f, 0.2f, 0.4f);
 }
 
-void pgl_update(System& system){};
+void pgl_update(System& system) {
+    system.camera.canvas.color = system.store["screen_color"];
+};
 
 void pgl_render(System& system) {
     system.framebuffers("#main").color().depth().clear().bind();
@@ -58,13 +61,6 @@ void pgl_render(System& system) {
             .param("projection", system.camera.geometry.get_projection())
             .param("light_pos", vec3(5.f, 5.f, 5.f))
             .param("view_pos", system.camera.geometry.get_position()));
-    system.geometry.render(
-        geometry::extract_normals(geometry::Dodecahedron{}),
-        system.content.shader("debug_dash.vs", "debug_dash.fs")
-            .param("world", glm::translate(vec3(3.f, 0.f, 3.f)))
-            .param("view", system.camera.geometry.get_view())
-            .param("projection", system.camera.geometry.get_projection())
-            .param("color", Color(0.f, 1.f, 0.f, 1.f)));
 
     system.postprocess("#main").with("grayscale.fs").resulting("#grayscale");
 
--- src/graphics/opengl/state.h
+++ src/graphics/opengl/state.h
@@ -4,35 +4,87 @@
 #define GLFW_INCLUDE_GLU
 #include <GLFW/glfw3.h>
 
+#include "common.h"
+
 class GpuState {
 public:
+    enum class BlendMode {
+        Zero = GL_ZERO,
+        One = GL_ONE,
+        SrcColor = GL_SRC_COLOR,
+        OneMinusSrcColor = GL_ONE_MINUS_SRC_COLOR,
+        DstColor = GL_DST_COLOR,
+        OneMinusDstColor = GL_ONE_MINUS_DST_COLOR,
+        SrcAlpha = GL_SRC_ALPHA,
+        OneMinusSrcAlpha = GL_ONE_MINUS_SRC_ALPHA,
+        DstAlpha = GL_DST_ALPHA,
+        OneMinusDstAlpha = GL_ONE_MINUS_DST_ALPHA,
+        ConstantColor = GL_CONSTANT_COLOR,
+        OneMinusConstantColor = GL_ONE_MINUS_CONSTANT_COLOR,
+        ConstantAlpha = GL_CONSTANT_ALPHA,
+        OneMinusConstantAlpha = GL_ONE_MINUS_CONSTANT_ALPHA,
+        SrcAlphaSaturate = GL_SRC_ALPHA_SATURATE,
+        Src1_Color = GL_SRC1_COLOR,
+        OneMinusSrc1Color = GL_ONE_MINUS_SRC1_COLOR,
+        Src1Alpha = GL_SRC1_ALPHA,
+        OneMinusSrc1Alpha = GL_ONE_MINUS_SRC1_ALPHA
+    };
+
+    enum class BlendEquation {
+        Add = GL_FUNC_ADD,
+        Subtract = GL_FUNC_SUBTRACT,
+        ReverseSubtract = GL_FUNC_REVERSE_SUBTRACT,
+        Min = GL_MIN,
+        Max = GL_MAX
+    };
+
     enum class CullFaceMode {
         Front = GL_FRONT,
         Back = GL_BACK,
         FrontAndBack = GL_FRONT_AND_BACK
     };
 
+    GpuState& blend(BlendMode src = BlendMode::One,
+                    BlendMode dst = BlendMode::Zero,
+                    BlendEquation eq = BlendEquation::Add,
+                    Color color = {0.f, 0.f, 0.f, 0.f}) {
+        _blend = true;
+        _src_blend_mode = src;
+        _dst_blend_mode = dst;
+        _blend_equation = eq;
+        _blend_color = color;
+        return *this;
+    }
+
     GpuState& nodepth() {
         _depth_test = false;
         return *this;
     }
 
-    GpuState& cull_face(CullFaceMode mode = CullFaceMode::Back) {
-        _cull_face = true;
-        _cull_face_mode = mode;
+    GpuState& nodepth_write() { _depth_write = false; }
+
+    GpuState& nocull() {
+        _cull_face = false;
         return *this;
     }
 
     void bind() const {
-        if (bind_lock) {
-            PlayGlException(
-                "Cannot bind GpuState. Unbind previous state first.");
-        }
-
         bind_lock = true;
+
+        if (_blend) {
+            glEnable(GL_BLEND);
+            glBlendFunc(static_cast<i32>(_src_blend_mode),
+                        static_cast<i32>(_dst_blend_mode));
+            glBlendEquation(static_cast<i32>(_blend_equation));
+            glBlendColor(_blend_color.r, _blend_color.g, _blend_color.b,
+                         _blend_color.a);
+        } else {
+            glDisable(GL_BLEND);
+        }
 
         if (_depth_test) {
             glEnable(GL_DEPTH_TEST);
+            glDepthMask(_depth_write);
         } else {
             glDisable(GL_DEPTH_TEST);
         }
@@ -56,7 +108,15 @@ public:
 private:
     mutable bool bind_lock = false;
 
+    bool _blend = false;
+    BlendMode _src_blend_mode = BlendMode::One;
+    BlendMode _dst_blend_mode = BlendMode::Zero;
+    BlendEquation _blend_equation = BlendEquation::Add;
+    Color _blend_color = {0.f, 0.f, 0.f, 0.f};
+
     bool _depth_test = true;
-    bool _cull_face = false;
+    bool _depth_write = true;
+
+    bool _cull_face = true;
     CullFaceMode _cull_face_mode = CullFaceMode::Back;
 };
